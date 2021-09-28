f1d4fa2abb6dbcfe83976dc22dd288b8ba583caf
ingrid-inga <leidyingrid20@gmail.com>
2021-09-28 14:13:47 -0300

    First Commit - Challenge Alkemy

---
+++ src/main/java/ar/com/api/disneychallenge/disneychallenge/DisneychallengeApplication.java
@@ -0,0 +1,13 @@
+package ar.com.api.disneychallenge.disneychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneychallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneychallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/api/disneychallenge/disneychallenge/DisneychallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.api.disneychallenge.disneychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneychallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
